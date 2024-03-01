7dceaa17b8d4803e9223b67c2e8c7bc8b92d4daf
inhoru126 <inhoru126@gmail.com>
2024-03-02 01:58:33 +0900

    19 ~ 22강 : 스프링컨테이너 의마와 사용법

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,70 +1,44 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.reponse.UserResponse;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import com.group.libraryapp.service.fruit.FruitService;
+import com.group.libraryapp.service.user.UserService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final UserService userService;
+    private final FruitService fruitService;
 
-    public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public UserController(UserService userService, @Qualifier("main") FruitService fruitService) {
+        this.userService = userService;
+        this.fruitService=fruitService;
     }
 
     @PostMapping("/user") //POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        User user = new User(request.getName(), request.getAge());
-        String sql = "insert into user (name, age) values (?, ?)";
-        jdbcTemplate.update(sql, user.getName(),user.getAge());
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        String sql = "select * from user";
-        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
-            @Override
-            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                return new UserResponse(id, new User(name, age));
-            }
-        });
+      return userService.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request){
-        String readSql = "select * from user where id = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-        if(isUserNotExist){
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "update user set name = ? where id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+     userService.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name){
-        String readSql = "select * from user where name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-        if(isUserNotExist){
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "delete from user where name = ?";
-        jdbcTemplate.update(sql,name);
+       userService.deleteUser(name);
     }
 
     @GetMapping("/user/error-test")
---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,60 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.dto.user.reponse.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void saveUser(String name, Integer age) {
+        String sql = "insert into user (name, age) values (?, ?)";
+        User user = new User(name, age);
+        jdbcTemplate.update(sql, user.getName(), user.getAge());
+    }
+
+    public List<UserResponse> getUsers() {
+        String sql = "select * from user";
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id, new User(name, age));
+            }
+        });
+    }
+
+    public void updateUserName(String name, long id) {
+        String sql = "update user set name = ? where id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+    public void deleteUser(String name) {
+        String sql = "delete from user where name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+
+    public boolean isUserNotExist(Long id) {
+        String readSql = "select * from user where id = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+
+    public boolean isUserNotExist(String name) {
+        String readSql = "select * from user where name = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+    }
+}
